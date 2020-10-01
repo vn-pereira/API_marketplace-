65075cfb92eb7c72a135fc1586b0d270ec1c400e
Eduardo Pinheiro <3dup1n@gmail.com>
2020-09-30 21:29:11 -0300

    MVC structure and files in blank

---
+++ src/server.js
@@ -0,0 +1,38 @@
+"use strict";
+// Importing dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Initializing the express application instance
+const app = express();
+
+// Applying middleware's to express app
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Error 404 Handler middleware
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "NOT FOUND",
+  });
+});
+
+// Global Error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    success: false,
+    message: error.message,
+  });
+});
+
+// defining Port number
+const port = process.env.PORT || 3000;
+
+// server listener.
+app.listen(port, () => {
+  console.log("App Listening On:", port);
+});
