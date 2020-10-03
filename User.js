261b96c334501a1ba08197987e6db7f75f5833e5
Vanessa P <vanessa.pereira.o@hotmail.com>
2020-10-03 02:25:32 -0300

    add database user

---
+++ User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const customer = new mongoose.Schema ({
+    idCustomer: String,
+    nomeCustomer: String,
+    emailCustomer: String,
+    senhaCustomer: String,
+    cpfCustomer: Number,
+    telefoneCustomer: Number,
+    endereçoCustomer: String,
+});
+    module.exports = customer;
+    
